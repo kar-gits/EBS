ce2de97264a3b83f3f38914def9a67d0fccc618e
Yanagandula Karthik <kyanagandula@gmail.com>
2021-12-14 21:46:18 -0600

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,12 +1,12 @@
 //configurations required
 module.exports = {
-  database: "mongodb+srv://EBSAdmin:YF$fZtcFNf5G_cm@ebs-cluster.fdbo9.mongodb.net/EBS?retryWrites=true&w=majority",
+  database: "database_path",
   port: 3030,
-  secret: 'V6hWnHCSaHOY9unEjufSndna25G5OcZu',
-  access_key: "AKIATGOLB5WUHZKUDL55",
-  access_secret: "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",
-  algolia_api_key: "7c30920f765d1bdeb20a9dbb938e7975",
-  algolia_app_id: "IW0O07R0KJ",
+  secret: 'secret_key',
+  access_key: "aws_access_key",
+  access_secret: "aws_access_secret",
+  algolia_api_key: "api_key",
+  algolia_app_id: "app_id",
   algolia_index: "ebs"
   };
 
